43e7db6343396064fab99d50577f15486d8d5cda
MalonsoFrey <miss.dunaewa201141@gmail.com>
2025-05-29 17:53:07 +0400

    logo path fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,8 +3,7 @@ const Layout = ({ children }) => {
     <div className="flex flex-col items-center gap-4 max-w-3xl mx-auto p-4">
       <header>
         <h1 className="flex items-center text-2xl">
-          {" "}
-          <img src="../../public/logoIcon.svg" alt="logo icon" /> Мой To-Do List
+          <img src="/logoIcon.svg" alt="logo icon" /> Мой To-Do List
         </h1>
       </header>
       <main className="flex flex-col items-center gap-4">{children}</main>
